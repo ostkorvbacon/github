6703fac3ad428cf3f8b1f79e35f65095a4df9d39
ostkorvbacon <mr_nordgren@hotmail.com>
2021-02-11 04:18:11 -0800

    städ

--- DVGB02/test/server.c
+++ DVGB02/test/server.c
@@ -11,38 +11,47 @@
 #define BUF_SIZE 4096
 #define QUEUE_SIZE 10
 
+int web(int sa, char *buf);
+
 int main(int argc, char *arhv[])
 {
     int s, b, l, fd, sa, bytes, on = 1;
     char buf[BUF_SIZE];
-    struct sockaddr_in channel;
-    int sizechannel = sizeof(channel);
+    struct sockaddr_in serveraddr;
+    int addrlngt = sizeof(serveraddr);
     // Adress structure
-    memset(&channel, 0, sizeof(channel));
-    channel.sin_family = AF_INET;
-    channel.sin_addr.s_addr = htonl(INADDR_ANY);
-    channel.sin_port = htons(SERVER_PORT);
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVER_PORT);
     //Passive open
     s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(s == -1) printf("error socket");
+    if(s == -1) printf("Error socket");
 
     setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
 
-    b = bind(s, (struct sockaddr*) &channel, sizeof(channel));
-    if(b == -1) printf("error bind");
+    b = bind(s, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
+    if(b == -1) printf("Error bind");
     l = listen(s, QUEUE_SIZE);
-    if(l == -1) printf("error listen");
+    if(l == -1) printf("Error listen");
 
     //socket set up and bound, wait for conectiopn.
     while(1)
     {
-      sa = accept(s, (struct sockaddr *)&channel, (socklen_t*)&sizechannel);
-      if(sa == -1) printf("error accept");
+      sa = accept(s, (struct sockaddr *)&serveraddr, (socklen_t*)&addrlngt);
+      if(sa == -1) printf("Error accept");
+      web(sa, buf);
 
-      read(sa, buf, BUF_SIZE);
-      printf("Message from user %s\n", buf);
-      write(sa, buf, BUF_SIZE);
-      printf("Message sent back to user\n");
       close(sa);
     }
+}
+
+int web(int sa, char *buf)
+{
+  read(sa, buf, BUF_SIZE);
+  printf("Message from user %s\n", buf);
+  write(sa, buf, BUF_SIZE);
+  printf("Message sent back to user\n");
+
+  return 0;
 }
