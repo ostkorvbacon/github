1143696bb24251d900115d637632d94412f81c26
ostkorvbacon <mr_nordgren@hotmail.com>
2021-04-23 10:03:28 -0700

    steg 2 helt färdig

--- DVGC01/lab1/prog/parser.c
+++ DVGC01/lab1/prog/parser.c
@@ -33,8 +33,9 @@ static int  is_parse_ok=1;
 /* Simulate the token stream for a given program                      */
 /**********************************************************************/
 static int tokens[] = {program, id, '(', input, ',', output, ')', ';',
+var, id, ',', id, ',', id, ':', integer, ';',
+begin, id, assign, id, '+', id, '*', number, end, '.',
                '$' };
-
 /**********************************************************************/
 /*  Simulate the lexer -- get the next token from the buffer          */
 /**********************************************************************/
@@ -47,13 +48,28 @@ static int pget_token()
 /**********************************************************************/
 /*  PRIVATE METHODS for this OBJECT  (using "static" in C)            */
 /**********************************************************************/
+static void prog();
+static void prog_header();
+static void var_part();
+static void var_dec_list();
+static void var_dec();
+static void id_list();
+static void type();
 
+static void stat_part();
+static void stat_list();
+static void stat();
+static void assign_stat();
+static void expr();
+static void term();
+static void factor();
+static void operand();
 /**********************************************************************/
 /* The Parser functions                                               */
 /**********************************************************************/
 static void match(int t)
 {
-   if(DEBUG) printf("\n --------In match expected: %4d, found: %4d",
+   if(DEBUG) printf("\n -------- In match expected: %4d, found: %4d",
                     t, lookahead);
    if (lookahead == t) lookahead = pget_token();
    else {
@@ -66,12 +82,163 @@ static void match(int t)
 /**********************************************************************/
 /* The grammar functions                                              */
 /**********************************************************************/
-static void program_header()
+
+static void in(char * name)
 {
-   if (DEBUG) printf("\n *** In  program_header");
-   match(program); match(id); match('('); match(input);
-   match(','); match(output); match(')'); match(';');
-   }
+  printf("\n *** In   %s", name);
+}
+static void out(char * name)
+{
+  printf("\n *** Out  %s", name);
+}
+
+static void prog()
+{
+  if(DEBUG == 1 || DEBUG == 2) in("program");
+  if(lookahead == program)
+  {
+    prog_header();  if(DEBUG == 1 || DEBUG == 2) out("program_header");
+  }
+  if (lookahead == var)
+  {
+    var_part();  if(DEBUG == 1 || DEBUG == 2) out("var_part");
+  }
+  if (lookahead == begin)
+  {
+    stat_part();  if(DEBUG == 1 || DEBUG == 2) out("stat_part");
+  }
+}
+static void prog_header()
+{
+  if(DEBUG == 1 || DEBUG == 2) in("program_header");
+  match(program);
+  match(id);
+  match('(');
+  match(input);
+  match(',');
+  match(output);
+  match(')');
+  match(';');
+}
+static void var_part()
+{
+  if(DEBUG == 1 || DEBUG == 2) in("var_part");
+  match(var);
+  var_dec_list(); if(DEBUG == 2) out("var_dec_list");
+}
+static void var_dec_list()
+{
+  if(DEBUG == 2) in("var_dec_list");
+  var_dec(); if(DEBUG == 2) out("var_dec");
+  if(lookahead == id)
+  {
+    var_dec(); if(DEBUG == 2) out("var_dec");
+  }
+}
+static void var_dec()
+{
+  if(DEBUG == 2) in("var_dec");
+  id_list(); if(DEBUG == 2) out("id_list");
+  match(':');
+  type(); if(DEBUG == 2) out("type");
+  match(';');
+
+}
+static void id_list()
+{
+  //id | [id list] , id
+  if(DEBUG == 2) in("id_list");
+  match(id);
+  if(lookahead == ',')
+  {
+    match(',');
+    id_list(); if(DEBUG == 2) out("id_list");
+  }
+}
+static void type()
+{
+  //integer | real | boolean
+  if(DEBUG == 2) in("type");
+  if(lookahead == integer)
+    match(integer);
+  else if(lookahead == real)
+    match(real);
+  else if(lookahead == boolean)
+    match(boolean);
+}
+static void stat_part()
+{
+  if(DEBUG == 1 || DEBUG == 2) in("var_dec_list");
+  match(begin);
+  stat_list(); if(DEBUG == 2) out("stat_list");
+  match(end);
+  match('.');
+}
+static void stat_list()
+{
+  if(DEBUG == 2) in("stat_list");
+  stat(); if(DEBUG == 2) out("stat");
+  if(lookahead == ';')
+  {
+    match(';');
+    stat_list(); if(DEBUG == 2) out("stat_list");
+  }
+}
+static void stat()
+{
+  if(DEBUG == 2) in("stat");
+  assign_stat(); if(DEBUG == 2) out("assign_stat");
+}
+static void assign_stat()
+{
+  if(DEBUG == 2) in("assign_stat");
+  match(id);
+  match(assign);
+  expr(); if(DEBUG == 2) out("expr");
+}
+static void expr()
+{
+  if(DEBUG == 2) in("expr");
+  term();
+  if(lookahead == '+')
+  {
+    match('+');
+    expr(); expr("if(DEBUG == 2) out");
+  }
+}
+static void term()
+{
+  if(DEBUG == 2) in("term");
+  factor();
+  if(lookahead == '*')
+  {
+    match('*');
+    term(); if(DEBUG == 2) out("term");
+  }
+
+}
+static void factor()
+{
+  if(DEBUG == 2) in("factor");
+  if(lookahead == '(')
+  {
+    match('(');
+    expr(); if(DEBUG == 2) out("expr");
+    match(')');
+  }
+  else
+  {
+    operand();  if(DEBUG == 2) out("operand");
+  }
+}
+static void operand()
+{
+  if(DEBUG == 2) in("operand");
+  if(lookahead == id)
+    match(id);
+  else if(lookahead == number)
+    match(number);
+}
 
 /**********************************************************************/
 /*  PUBLIC METHODS for this OBJECT  (EXPORTED)                        */
@@ -79,9 +246,9 @@ static void program_header()
 
 int parser()
 {
-   if (DEBUG) printf("\n *** In  parser");
+   if (DEBUG) printf("\n *** if(DEBUG == 2) in  parser");
    lookahead = pget_token();       // get the first token
-   program_header();               // call the first grammar rule
+   prog();               // call the first grammar rule
    return is_parse_ok;             // status indicator
    }
 
