90d39100be2822674cebfc613d3f2a81ef68912b
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2021-10-22 15:33:09 +0200

    Erik_v3

--- task-sched.c
+++ task-sched.c
@@ -87,6 +87,36 @@ void sort2() {
 }
 
 
+void remlate(){
+	for (int l=0;l<i;l++) {
+			if (queue[l]!=NULL) fprintf(fp, "Time: %ld Abs Deadline: %ld Index:%ld\n",get_time(),queue[l]->abs_deadline, queue[l]->index);
+
+		if (queue[l]!=NULL && queue[l]->abs_deadline<=get_time()) {
+
+			for(int index=l;index<i;index++){
+			 discard_task(queue[index]);
+					queue[index]=queue[index+1];
+
+			}
+
+			queue[i]=NULL;
+			if (i!=0) i--;
+		}
+
+	}
+
+	if (queue[i]!=NULL) fprintf(fp, "Time: %ld Abs Deadline: %ld Index:%ld\n",get_time(),queue[i]->abs_deadline, queue[i]->index);
+	if (queue[i]!=NULL && queue[i]->abs_deadline<=get_time()){
+ 	discard_task(queue[i]);
+		queue[i]=NULL;
+		if (i!=0) i--;
+	}
+
+
+}
+
+/////////////////////////////////////////RM SOFT
+
 i64 sched_with_rm(void) {
 
 	if (tasks_remain()){
@@ -133,7 +163,7 @@ i64 sched_with_rm(void) {
 			if (i!=0) i--;
 		}
 
-		else if (queue[i]!=NULL && peek_task()->deadline>queue[i]->deadline /*&& ((queue[i]->arrival/queue[i]->deadline)<=(peek_task()->arrival/peek_task()->deadline))*/) {
+		else if (queue[i]!=NULL && peek_task()->deadline>queue[i]->deadline ) {
 			sch_task* temp2=preempt_task();
 			process_task(queue[i]);
 			queue[i]=temp2;
@@ -150,15 +180,87 @@ i64 sched_with_rm(void) {
 	return 0;
 }
 
+/////////////////////////////////////////RM HARD
 
 i64 sched_with_rm_hard(void) {
-	if (0) { // Check if there are more tasks to be processed
+
+	if (tasks_remain()){
+
+		sch_task* temp=get_current_sch_task();
+		if (i==0 && queue[i]==NULL) {
+			queue[i]=temp;
+			temp=get_current_sch_task();
+
+		}
+			while (temp) {
+				i++;
+				queue[i]=temp;
+				temp=get_current_sch_task();
+			}
+
+
+
+
+		if (i!=0 || 1){
+		fprintf(fp,"\n");
+		fprintf(fp,"%ld\n",get_time());
+		fprintf(fp,"%p\n",peek_task());
+		//if (peek_task()) fprintf(fp, "%ld\n",peek_task()->arrival );
+
+		for (int w=0;w<=i;w++)	{
+
+			fprintf(fp,"task: %p i=%d\n", queue[w],i);
+			if (queue[w]) fprintf(fp, "arrival: %ld Deadline: %ld\n",queue[w]->arrival,queue[w]->abs_deadline);
+		}
+		fprintf(fp,"\n");
+}
+		sort();
+		remlate();
+		for (int w=0;w<=i;w++)	{
+
+			fprintf(fp,"task: %p i=%d\n", queue[w],i);
+			if (queue[w]) fprintf(fp, "arrival: %ld Deadline: %ld\n",queue[w]->arrival,queue[w]->abs_deadline);
+		}
+
+
+	/*	if (queue[i]!=NULL && queue[i]->abs_deadline<=get_time()+queue[i]->duration){
+
+			discard_task(queue[i]);
+			queue[i]=NULL;
+			if (i!=0) i--;
+
+		}
+*/
+		if(!peek_task()){
+			process_task(queue[i]);
+			queue[i]=NULL;
+			if (i!=0) i--;
+		}
+		else if (peek_task()->abs_deadline<=(get_time())){
+				abort_task();
+				process_task(queue[i]);
+				queue[i]=NULL;
+				if (i!=0) i--;
+		}
+
+		else if (queue[i]!=NULL && peek_task()->deadline>queue[i]->deadline ) {
+			sch_task* temp2=preempt_task();
+			process_task(queue[i]);
+			queue[i]=temp2;
+		}
+
+
+//printf("Slut 1\n\n");
 		return 1;
+
+
 	}
+//printf("Slut 0\n\n" );
+  fclose(fp);
 	return 0;
 }
 
-
+/////////////////////////////////////////EDF Soft
 
 i64 sched_with_edf(void) {
 
@@ -220,11 +322,70 @@ i64 sched_with_edf(void) {
 	return 0;
 
 }
+/////////////////////////////////////////EDF HARD
 
 i64 sched_with_edf_hard(void) {
-	if (0) { // Check if there are more tasks to be processed
+	if (tasks_remain()){
+		//printf("Start\n");
+		sch_task* temp=get_current_sch_task();
+
+//printf("Hit i=%d\n",i );
+	if (i==0 && queue[i]==NULL) {
+			queue[i]=temp;
+			temp=get_current_sch_task();
+
+		}
+
+			while (temp) {
+				i++;
+				queue[i]=temp;
+				temp=get_current_sch_task();
+			}
+
+
+
+
+		/*if (i!=0 || 1){
+		fprintf(fp,"\n");
+		fprintf(fp,"%ld\n",get_time());
+		fprintf(fp,"%p\n",peek_task());
+		//if (peek_task()) fprintf(fp, "%ld\n",peek_task()->arrival );
+
+		for (int w=0;w<=i;w++)	{
+
+			fprintf(fp,"task: %p i=%d\n", queue[w],i);
+			if (queue[w]) fprintf(fp, "%ld %ld\n",queue[w]->arrival,queue[w]->abs_deadline);
+		}
+		fprintf(fp,"\n");
+}*/
+		sort2();
+		remlate();
+	/*	for (int w=0;w<=i;w++)	{
+
+			fprintf(fp,"task: %p i=%d\n", queue[w],i);
+			if (queue[w]) fprintf(fp, "%ld %ld\n",queue[w]->arrival,queue[w]->abs_deadline);
+		}*/
+
+ 	if(!peek_task()){
+			process_task(queue[i]);
+			queue[i]=NULL;
+			if (i!=0) i--;
+		}
+		else if (peek_task()->abs_deadline<=(get_time())){
+				abort_task();
+				process_task(queue[i]);
+				queue[i]=NULL;
+				if (i!=0) i--;
+		}
+
+		else if (queue[i]!=NULL && peek_task()->abs_deadline>queue[i]->abs_deadline) {
+			sch_task* temp2=preempt_task();
+			process_task(queue[i]);
+			queue[i]=temp2;
+		}
 		return 1;
 	}
+	fclose(fp);
 	return 0;
 }
 
@@ -235,7 +396,7 @@ int main(int argc, char *argv[]) {
 	i64 run_tests = 0;
 	i64 run_prog1_prog2 = 0;
 
-	if(run_tests || 1) {
+	if(run_tests) {
 
 		init_task_buf_state();
 		init_custom_prog(STD_DURATION, "test-comp-rm");
@@ -251,14 +412,14 @@ int main(int argc, char *argv[]) {
 		printf("OK Hit\n" );
 		run_custom_prog(sched_with_edf);
 	}
-	if(run_tests && 0) {
+	if(run_tests || 0) {
 		init_task_buf_state();
 		init_custom_prog(STD_DURATION, "test-comp-rm-hard");
 		add_custom_task(0, 25, 50);
 		add_custom_task(0, 30, 75);
 		run_custom_prog(sched_with_rm_hard);
 	}
-	if(run_tests && 0) {
+	if(run_tests || 1) {
 		init_task_buf_state();
 		init_custom_prog(STD_DURATION, "test-comp-edf-hard");
 		add_custom_task(0, 25, 50);
