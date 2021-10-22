10d199fb5668785e9b0d59f97b1e38df79974259
eriksven107 <57899290+eriksven107@users.noreply.github.com>
2021-10-22 17:47:15 +0200

    Add files via upload

---
+++ analyze.c
@@ -0,0 +1,49 @@
+#import <stdio.h>
+
+#define LENGTH 7
+
+
+int read(char* file, int rows) {
+
+  int i=0;
+
+  int temp[LENGTH];
+  int index=0;
+  int arrival=0;
+  int proc_time=0;
+  int period=0;
+  int abs_deadline=0;
+  int finished=0;
+  int observed_proc_time=0;
+
+  int total_response=0;
+int total_lateness=0;
+
+FILE *fp=fopen(file,"r");
+printf("File: %s\n",file);
+while (i<rows) {
+    fscanf(fp,"f:%d:%d:%d:%d:%d:%d:%d\n",&temp[0],&temp[1],&temp[2],&temp[3],&temp[4],&temp[5],&temp[6]);
+    total_response=total_response+temp[5]-temp[1];
+    total_lateness=total_lateness+temp[5]-temp[3];
+    i++;
+
+    //printf("i: %d total response time: %d total lateness: %d \n",i,total_response,total_lateness);
+
+}
+i--;
+printf("average response time: %d average lateness: %d \n\n",total_response/i,total_lateness/i);
+fclose(fp);
+return 0;
+
+}
+
+int main() {
+
+read("trace-prog1-edf.data",1024);
+read("trace-prog1-rm.data",1024);
+read("trace-prog2-edf.data",1025);
+read("trace-prog2-rm.data",1025);
+  return 0;
+}
+//1-edf:38 9069
+//1-rm: 39 9071
